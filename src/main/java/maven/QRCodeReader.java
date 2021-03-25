0ed785b0de96644fa1fd2aa55e923b324025d10d
Lisam <leble@mail.hs-ulm.de>
2021-03-25 16:03:11 +0100

    Changes
    Goodbye World and QR Code Reader

---
+++ src/main/java/maven/QRCodeReader.java
@@ -0,0 +1,47 @@
+package maven;
+
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLDecoder;
+
+public class QRCodeReader {
+
+    public static String decodeQRCode(File qrCodeimage) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(qrCodeimage);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+        try {
+            Result result = new MultiFormatReader().decode(bitmap);
+            System.out.println(result.getText());
+            return result.getText();
+        } catch (NotFoundException e) {
+            System.out.println("There is no QR code in the image");
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+        	 String resourceName = "qr.png";
+             URL resource = ClassLoader.getSystemResource(resourceName);
+             String configPath = URLDecoder.decode(resource.getFile(), "UTF-8");
+        	
+            File file = new File("qr.png");
+            String decodedText = decodeQRCode(file);
+            if(decodedText == null) {
+                System.out.println("No QR Code found in the image");
+            } else {
+                System.out.println("Decoded text = " + decodedText);
+            }
+        } catch (Exception e) {
+            System.out.println("Could not decode QR Code, IOException :: " + e.getMessage());
+        }
+    }
+}
--- src/test/java/test/QRTest.java
+++ src/test/java/test/QRTest.java
@@ -3,20 +3,26 @@ package test;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import maven.QRCodeReader;
+import java.net.URLDecoder;
+import java.net.URL;
 
 import org.junit.Test;
 
 public class QRTest {
-
-	@Test
-	public void test() {
-		String resourceName = "qr.png";
-
-		ClassLoader classLoader = getClass().getClassLoader();
-		File file = new File(classLoader.getResource(resourceName).getFile());
-		String absolutePath = file.getAbsolutePath();
-		System.out.println(absolutePath);
-		assertTrue(absolutePath.endsWith(".png"));
-	}
+    @Test
+    public void test() throws Exception {
+        String resourceName = "qr.png";
+        URL resource = ClassLoader.getSystemResource(resourceName);
+        String configPath = URLDecoder.decode(resource.getFile(), "UTF-8");
+        
+        //ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(configPath);
+        String absolutePath = file.getAbsolutePath();
+        System.out.println(absolutePath);
+        assertTrue(absolutePath.endsWith(".png"));
+        //QRCodeReader.decodeQRCode(file);
+        QRCodeReader.decodeQRCode(file);
+    }
 
 }
